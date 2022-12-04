a1007d37ad3adb0e577290ac808607c067692a3b
Animesh Sinha <AnimeshSinha1309@gmail.com>
2022-12-05 03:42:31 +0530

    Bindings with python work, classes and all
    The class in C++ should be working in python. The function call should pass the test, well almost, not all the functions are hooked in yet.

---
+++ bindings/game_2048.cpp
@@ -0,0 +1,9 @@
+#include <pybind11/pybind11.h>
+#include "environments/game_2048/game_state.hpp"
+
+PYBIND11_MODULE(game_2048, m) {
+    m.doc() = "A simulation environment for the 2048 game";
+    pybind11::class_<GameState>(m, "GameState2048")
+        .def(pybind11::init<int>())
+        .def("score", &GameState::score);
+}
---
+++ python/test_env.py
@@ -0,0 +1,4 @@
+import game_2048
+
+board = game_2048.GameState2048(4)
+print(board.score())
