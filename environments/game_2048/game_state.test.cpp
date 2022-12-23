0558f80384dcdcf4934ac3567f74538d87ed6c8e
Animesh Sinha <AnimeshSinha1309@gmail.com>
2022-12-23 13:05:12 +0530

    Reasonable quality tests
    Higher quality testing, with merges and stuff tested. All tests written
    in much better code quality.

--- environments/game_2048/game_state.test.cpp
+++ environments/game_2048/game_state.test.cpp
@@ -2,77 +2,111 @@
 #include <gmock/gmock.h>
 #include "game_state.hpp"
 
-TEST(GameStateTest, TestBasicMoveUp) {
-    GameState2048<4> game({
-        { 0,  0,  0,  0},
-        { 0, 32, 16,  0},
-        { 0, 64,  8,  0},
-        { 0,  0,  0,  0},
-    });
 
-    game.move(GameState2048<4>::Move::UP);
+class TestBasicMovementActions : public ::testing::TestWithParam<GameState2048<4>::Move> {
+protected:
+    GameState2048<4>::Move move;
+};
+
+TEST_P(TestBasicMovementActions, TestBasicMoveUp) {
+    GameState2048<4> game({
+            { 0,  0,  0,  0},
+            { 0, 32, 16,  0},
+            { 0, 64,  8,  0},
+            { 0,  0,  0,  0},
+    });
+    decltype(game)::Move move = GetParam();
+
+    game.move(move);
     std::vector<std::vector<int>> obtained_state = game.get_board();
 
-    std::vector<std::vector<int>> expected_state = {
-        { 0, 32, 16,  0},
-        { 0, 64,  8,  0},
-        { 0,  0,  0,  0},
-        { 0,  0,  0,  0},
-    };
+    std::vector<std::vector<int>> expected_state;
+    switch (move) {
+        case decltype(move)::UP:
+            expected_state = {
+                    { 0, 32, 16,  0},
+                    { 0, 64,  8,  0},
+                    { 0,  0,  0,  0},
+                    { 0,  0,  0,  0},
+            };
+            break;
+        case decltype(move)::DOWN:
+            expected_state = {
+                    { 0,  0,  0,  0},
+                    { 0,  0,  0,  0},
+                    { 0, 32, 16,  0},
+                    { 0, 64,  8,  0},
+            };
+            break;
+        case decltype(move)::LEFT:
+            expected_state = {
+                    { 0,  0,  0,  0},
+                    {32, 16,  0,  0},
+                    {64,  8,  0,  0},
+                    { 0,  0,  0,  0},
+            };
+            break;
+        case decltype(move)::RIGHT:
+            expected_state = {
+                    { 0,  0,  0,  0},
+                    { 0,  0, 32, 16},
+                    { 0,  0, 64,  8},
+                    { 0,  0,  0,  0},
+            };
+            break;
+        default:
+            break;
+    }
 
+    int count_of_small_tiles = 0;
+    for (auto &row : obtained_state) {
+        for (auto &element : row) {
+            if (element == 2 || element == 4) {
+                count_of_small_tiles++;
+                element = 0;
+            }
+        }
+    }
     EXPECT_THAT(obtained_state, testing::ElementsAreArray(expected_state));
+    EXPECT_EQ(count_of_small_tiles, 1);
 }
 
-TEST(GameStateTest, TestBasicMoveDown) {
-    GameState2048<4> game({
-       { 0,  0,  0,  0},
-       { 0, 32, 16,  0},
-       { 0, 64,  8,  0},
-       { 0,  0,  0,  0},
-    });
-    game.move(GameState2048<4>::Move::DOWN);
-    std::vector<std::vector<int>> obtained_state = game.get_board();
-    std::vector<std::vector<int>> expected_state = {
-        { 0,  0,  0,  0},
-        { 0,  0,  0,  0},
-        { 0, 32, 16,  0},
-        { 0, 64,  8,  0},
-    };
-    EXPECT_THAT(obtained_state, testing::ElementsAreArray(expected_state));
-}
+INSTANTIATE_TEST_SUITE_P(
+        TestBasicMovements,
+        TestBasicMovementActions,
+        ::testing::Values(
+                GameState2048<4>::Move::UP,
+                GameState2048<4>::Move::DOWN,
+                GameState2048<4>::Move::LEFT,
+                GameState2048<4>::Move::RIGHT
+                ));
 
-TEST(GameStateTest, TestBasicMoveLeft) {
-    GameState2048<4> game({
-       { 0,  0,  0,  0},
-       { 0, 32, 16,  0},
-       { 0, 64,  8,  0},
-       { 0,  0,  0,  0},
-    });
-    game.move(GameState2048<4>::Move::LEFT);
-    std::vector<std::vector<int>> obtained_state = game.get_board();
-    std::vector<std::vector<int>> expected_state = {
-        { 0,  0,  0,  0},
-        {32, 16,  0,  0},
-        {64,  8,  0,  0},
-        { 0,  0,  0,  0},
-    };
-    EXPECT_THAT(obtained_state, testing::ElementsAreArray(expected_state));
-}
 
-TEST(GameStateTest, TestBasicMoveRight) {
+TEST(TestMovementAndMerges, TestMoveUp) {
     GameState2048<4> game({
-       { 0,  0,  0,  0},
-       { 0, 32, 16,  0},
-       { 0, 64,  8,  0},
-       { 0,  0,  0,  0},
+            {16,  0, 32, 32},
+            {16, 32, 64, 16},
+            {16,  0, 64,  0},
+            {16, 32, 64,  0},
     });
-    game.move(GameState2048<4>::Move::RIGHT);
+    game.move(decltype(game)::Move::DOWN);
     std::vector<std::vector<int>> obtained_state = game.get_board();
+
     std::vector<std::vector<int>> expected_state = {
-        { 0,  0,  0,  0},
-        { 0,  0, 32, 16},
-        { 0,  0, 64,  8},
-        { 0,  0,  0,  0},
+            { 0,  0,   0,  0},
+            { 0,  0,  32,  0},
+            {32,  0,  64, 32},
+            {32, 64, 128, 16},
     };
+    int count_of_small_tiles = 0;
+    for (auto &row : obtained_state) {
+        for (auto &element : row) {
+            if (element == 2 || element == 4) {
+                count_of_small_tiles++;
+                element = 0;
+            }
+        }
+    }
     EXPECT_THAT(obtained_state, testing::ElementsAreArray(expected_state));
+    EXPECT_EQ(count_of_small_tiles, 1);
 }
